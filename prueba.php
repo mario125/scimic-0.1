3a446f301d00e72ff3a4b2b3716dd95b1b04e01e
aldair saguma <aldairsaguma@gmail.com>
2018-11-15 12:43:34 -0500

    c3

--- prueba.php
+++ prueba.php
@@ -18,3 +18,23 @@ for ($i=0; $i <13 ; $i++) {
 </html>
 
 
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>Hola</h1>
+	<h2>Hola papu</h2>
+<?php
+echo"DSASADSA";
+for ($i=0; $i <13 ; $i++) { 
+	echo "a" + $i;
+}
+
+?>
+
+</body>
+</html>
+
+
