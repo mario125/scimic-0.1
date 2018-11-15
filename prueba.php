d845e0e1bb3e1b69492fbf76c6701f2186df1594
aldair saguma <aldairsaguma@gmail.com>
2018-11-15 12:35:40 -0500

    commit33333

--- prueba.php
+++ prueba.php
@@ -5,7 +5,26 @@
 </head>
 <body>
 	<h1>Hola</h1>
+<?php
+echo"DSASADSA";
+for ($i=0; $i <10 ; $i++) { 
+	echo "a" + $i;
+}
 
+?>
+
+</body>
+</html>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>Hola</h1>
+<?php
 echo"DSASADSA";
 for ($i=0; $i <10 ; $i++) { 
 	echo "a" + $i;
